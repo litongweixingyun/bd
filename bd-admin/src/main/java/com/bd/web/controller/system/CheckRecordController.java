3d8e7eb4e73e874f14354dcccae383f963a19dd8
git <2387509914@qq.com>
2019-07-11 18:01:59 +0800

    ltw

--- bd-admin/src/main/java/com/bd/web/controller/system/CheckRecordController.java
+++ bd-admin/src/main/java/com/bd/web/controller/system/CheckRecordController.java
@@ -1,6 +1,8 @@
 package com.bd.web.controller.system;
 
 import java.util.List;
+
+import com.bd.system.domain.SysUser;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,26 +23,24 @@ import com.bd.common.utils.poi.ExcelUtil;
 
 /**
  * 检查记录 信息操作处理
- * 
- * @author luxuewei
- * @date 2019-04-21
+ * @author qingjian
+ * @date 2019-07-05
  */
 @Controller
 @RequestMapping("/system/checkRecord")
-public class CheckRecordController extends BaseController
-{
-    private String prefix = "system/checkRecord";
-	
+public class CheckRecordController extends BaseController {
+	private String prefix = "system/checkRecord";
+
 	@Autowired
 	private ICheckRecordService checkRecordService;
-	
+
 	@RequiresPermissions("system:checkRecord:view")
 	@GetMapping()
 	public String checkRecord()
 	{
-	    return prefix + "/checkRecord";
+		return prefix + "/checkRecord";
 	}
-	
+
 	/**
 	 * 查询检查记录列表
 	 */
@@ -50,33 +50,34 @@ public class CheckRecordController extends BaseController
 	public TableDataInfo list(CheckRecord checkRecord)
 	{
 		startPage();
-        List<CheckRecord> list = checkRecordService.selectCheckRecordList(checkRecord);
+		List<CheckRecord> list = checkRecordService.selectCheckRecordList(checkRecord);
 		return getDataTable(list);
 	}
-	
-	
+
+
 	/**
 	 * 导出检查记录列表
 	 */
+	@Log(title = "检查记录", businessType = BusinessType.DELETE)
 	@RequiresPermissions("system:checkRecord:export")
-    @PostMapping("/export")
-    @ResponseBody
-    public AjaxResult export(CheckRecord checkRecord)
-    {
-    	List<CheckRecord> list = checkRecordService.selectCheckRecordList(checkRecord);
-        ExcelUtil<CheckRecord> util = new ExcelUtil<CheckRecord>(CheckRecord.class);
-        return util.exportExcel(list, "checkRecord");
-    }
-	
+	@PostMapping("/export")
+	@ResponseBody
+	public AjaxResult export(CheckRecord checkRecord)
+	{
+		List<CheckRecord> list = checkRecordService.selectCheckRecordList(checkRecord);
+		ExcelUtil<CheckRecord> util = new ExcelUtil<CheckRecord>(CheckRecord.class);
+		return util.exportExcel(list, "检查记录");
+	}
+
 	/**
 	 * 新增检查记录
 	 */
 	@GetMapping("/add")
 	public String add()
 	{
-	    return prefix + "/add";
+		return prefix + "/add";
 	}
-	
+
 	/**
 	 * 新增保存检查记录
 	 */
@@ -85,7 +86,7 @@ public class CheckRecordController extends BaseController
 	@PostMapping("/add")
 	@ResponseBody
 	public AjaxResult addSave(CheckRecord checkRecord)
-	{		
+	{
 		return toAjax(checkRecordService.insertCheckRecord(checkRecord));
 	}
 
@@ -97,9 +98,9 @@ public class CheckRecordController extends BaseController
 	{
 		CheckRecord checkRecord = checkRecordService.selectCheckRecordById(checkRecordId);
 		mmap.put("checkRecord", checkRecord);
-	    return prefix + "/edit";
+		return prefix + "/edit";
 	}
-	
+
 	/**
 	 * 修改保存检查记录
 	 */
@@ -108,10 +109,10 @@ public class CheckRecordController extends BaseController
 	@PostMapping("/edit")
 	@ResponseBody
 	public AjaxResult editSave(CheckRecord checkRecord)
-	{		
+	{
 		return toAjax(checkRecordService.updateCheckRecord(checkRecord));
 	}
-	
+
 	/**
 	 * 删除检查记录
 	 */
@@ -120,8 +121,8 @@ public class CheckRecordController extends BaseController
 	@PostMapping( "/remove")
 	@ResponseBody
 	public AjaxResult remove(String ids)
-	{		
+	{
 		return toAjax(checkRecordService.deleteCheckRecordByIds(ids));
 	}
-	
+
 }
